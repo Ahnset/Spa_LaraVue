1e506e6a6f4b3b55ec53520535b4d778e57c5993
MaggicBones <ahnset24@gmail.com>
2018-08-07 15:20:20 +0000

    Category API Completed!

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name', 'slug'];
+
+    // Laravel builtin Function for API route Parameter.
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
